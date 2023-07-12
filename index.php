60bce9ebacf8116867b678f53ae9e4d3301f73ee
Kenny <kenneth.kep@gmail.com>
2023-07-11 21:29:57 -0600

    first commit

---
+++ index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MathAdventure Platform</title>
+    <link rel="stylesheet" href="./assets/css/style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Ubuntu:ital,
+    wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap" rel="stylesheet">
+
+
+</head>
+<body>
+
+    <main>
+
+        <div class="contenedor-todo">
+            <div class="caja-trasera">
+                <div class="caja-trasera-login">
+                    <h3>¿Ya tienes una cuenta?</h3>
+                    <p>Inicia sesión para entrar en la página</p>
+                    <button id="btn-iniciar-sesion">Iniciar Sesión</button>
+                </div>
+                <div class="caja-trasera-register">
+                    <h3>¿Aún no tienes una cuenta?</h3>
+                    <p>Regístrate iniciar sesión</p>
+                    <button id="btn-registrarse">Regístrarse</button>
+                </div>
+            </div>
+            
+                <!-- Formulario de Loggin y Registro -->
+            <div class="contenedor-login-register">
+                <!-- Loggin -->
+                <form action="" class="formulario-login">
+                    <h2>Inciar sesión</h2>
+                    <input type="text" placeholder="Correo">
+                    <input type="password" placeholder="Contraseña">
+                    <button>Entrar</button>
+                </form>
+                <!-- Registro -->
+                <form action="" class="formulario-register">
+                    <h2>Regístrarse</h2>
+                    <input type="text" placeholder="Nombre completo">
+                    <input type="text" placeholder="Correo">
+                    <input type="text" placeholder="Usuario">
+                    <input type="password" placeholder="Contraseña">
+                    <button>Regístrarse</button>
+                </form>
+            </div>
+        </div>
+    </main>
+
+    <script src="./assets/js/script.js"></script>
+    
+</body>
+</html>
