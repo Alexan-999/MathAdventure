ea28ad837496b870fa48e548e921ec6cba25f211
Kenny <kenneth.kep@gmail.com>
2023-07-12 20:26:05 -0600

    verify if email and user is repeated

--- php/registro_usuario_be.php
+++ php/registro_usuario_be.php
@@ -6,9 +6,40 @@
     $correo = $_POST['correo'];
     $usuario = $_POST['usuario'];
     $password = $_POST['password'];
+    /* Encriptado de password */
+    $password = hash('sha512', $password);
 
     $query = "INSERT INTO usuarios(nombre_completo,correo,usuario,password)
               VALUES('$nombre_completo','$correo','$usuario','$password')";
+
+
+/* Verificar si el correo se repite */
+    $verificar_correo = mysqli_query($conexion,"SELECT * FROM usuarios WHERE correo='$correo'");
+
+    if(mysqli_num_rows($verificar_correo) > 0) {
+           echo '
+            <script>
+                alert("Este correo ya ha sido registrado");
+                window.location = "../index.php";
+            </script>
+           ';
+           exit();
+           mysqli_close($conexion);
+    }
+
+/* Verificar si el Usuario se repite */
+    $verificar_usuario = mysqli_query($conexion,"SELECT * FROM usuarios WHERE usuario='$usuario'");
+
+    if(mysqli_num_rows($verificar_usuario) > 0) {
+        echo '
+            <script>
+                alert("Este usuario ya ha sido registrado, por favor intenta de nuevo");
+                window.location = "../index.php";
+            </script>
+        ';
+        exit();
+        mysqli_close($conexion);
+    }
 
     $ejecutar = mysqli_query($conexion,$query);
 
